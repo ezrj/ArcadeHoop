5f2eaa36dda33417610562f3231f5cf2257bad7f
MediocreWizard <>
2024-04-18 23:00:42 -0500

    Add motor interrupts, partial button implementation

--- Arduino/display_button_motor/display_button_motor.ino
+++ Arduino/display_button_motor/display_button_motor.ino
@@ -1,13 +1,61 @@
+#include <Arduino.h>
+
 #include <Adafruit_NeoPixel.h>
 #define PIN        6
 #define NUMPIXELS 256
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-#define MOTOR_DIRECTION_IN1 2
-#define MOTOR_DIRECTION_IN2 3
-#define MOTOR_ENABLE 4
+#include <RotaryEncoder.h>
+#define MOTOR_DIRECTION_IN1 4
+#define MOTOR_DIRECTION_IN2 5
+#define MOTOR_ENABLE 7
+#define MOTOR_ENCODER_IN1 2  // Must be digital 2 & 3
+#define MOTOR_ENCODER_IN2 3
+RotaryEncoder *encoder = nullptr;
 
-#define HOOP_BUTTON 8
+void checkMotorPosition() {
+  static int motor_encoder_position = 0;
+
+  encoder->tick();
+
+  int newPos = encoder->getPosition();
+  if (motor_encoder_position != newPos) {
+    Serial.print("pos:");
+    Serial.print(newPos);
+    Serial.print(" dir:");
+    Serial.println((int)(encoder->getDirection()));
+    motor_encoder_position = newPos;
+
+    if (newPos < -250) {
+      digitalWrite(MOTOR_DIRECTION_IN1, HIGH);
+      digitalWrite(MOTOR_DIRECTION_IN2, LOW);
+    }
+    else if (newPos > 250) {
+      digitalWrite(MOTOR_DIRECTION_IN1, LOW);
+      digitalWrite(MOTOR_DIRECTION_IN2, HIGH);
+    }
+  }
+}
+
+
+#include <PinChangeInterrupt.h>
+#define HOOP_BUTTON 9
+
+void buttonInterrupt() {
+  if (digitalRead(9)) {
+    if (digitalRead(HOOP_BUTTON) == HIGH) { // PIN is active low
+      for (unsigned char i = 0; i < 255; ++i) {
+        if (i % 8 == 0) {
+          pixels.setPixelColor(i, pixels.Color(20, 0, 0));
+        }
+      }
+    }
+  } else if (digitalRead(10)) {
+  } else if (digitalRead(11)) {
+  }
+}
+
+#define BRIGHTNESS 5
 
 #define DELAYVAL 100
 unsigned long startTime;  //some global variables available anywhere in the program
@@ -15,6 +63,7 @@ unsigned long currentTime;
 //https://www.arduino.cc/reference/en/language/functions/time/millis/
 
 void setup() {
+  delay(8000);
   pixels.begin();
   pinMode(MOTOR_DIRECTION_IN1, OUTPUT);
   pinMode(MOTOR_DIRECTION_IN2, OUTPUT);
@@ -22,8 +71,21 @@ void setup() {
   pinMode(HOOP_BUTTON, INPUT);
   digitalWrite(MOTOR_DIRECTION_IN1, LOW);
   digitalWrite(MOTOR_DIRECTION_IN2, HIGH);
-  analogWrite(MOTOR_ENABLE, 255);
-  startTime= millis(); 
+  digitalWrite(MOTOR_ENABLE, 1);
+  startTime= millis();
+
+//  PCICR | = B00000001; // Enable interrupts on PB
+//  PCMSK0 |= B00001110; // Allow interrupts on pins 11, 10,
+
+  // Attach button interrupts
+  attachPCINT(digitalPinToPCINT(HOOP_BUTTON), buttonInterrupt, CHANGE);
+
+  // Attach motor encoder interrupts
+  pinMode(MOTOR_ENCODER_IN1, OUTPUT);
+  pinMode(MOTOR_ENCODER_IN2, OUTPUT);
+  encoder = new RotaryEncoder(MOTOR_ENCODER_IN1, MOTOR_ENCODER_IN2, RotaryEncoder::LatchMode::TWO03);
+  attachInterrupt(digitalPinToInterrupt(MOTOR_ENCODER_IN1), checkMotorPosition, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(MOTOR_ENCODER_IN2), checkMotorPosition, CHANGE);
 }
 
 bool numeric[10][40] = {
@@ -299,7 +361,7 @@ void drawChar(short num, unsigned char a, unsigned char ir, unsigned char ig, un
   default:
     index = 9;
     break;}
-  
+
   bool t = (num % 16 > 7) || (num % 16 < -7);
     //loops from 0-39
   for (unsigned char i=0; i<40; ++i) {
@@ -308,14 +370,14 @@ void drawChar(short num, unsigned char a, unsigned char ir, unsigned char ig, un
         j = 7 + (8 * (i / 8)) - i % 8;
     }
     Serial.println("The character is 'a'");
-    pixels.setPixelColor(i + num, pixels.Color(1 * newmatrix[index][j], 1 * newmatrix[index][j], 1 * newmatrix[index][j]));
+    pixels.setPixelColor(i + num, pixels.Color(BRIGHTNESS * newmatrix[index][j], BRIGHTNESS * newmatrix[index][j], BRIGHTNESS * newmatrix[index][j]));
   }
 }
 
 //ight up the entire board with a given color
 void lightUpBoard(uint8_t r, uint8_t g, uint8_t b) {
     for(int i = 0; i < NUMPIXELS; i++) {
-        pixels.setPixelColor(i, pixels.Color(r, g, b));
+        pixels.setPixelColor(i, pixels.Color(BRIGHTNESS * r, BRIGHTNESS * g, BRIGHTNESS * b));
     }
     pixels.show(); //has its own show
 }
@@ -326,20 +388,20 @@ void symmetrical() {
     for (int j = 0; j < 32; j++)  {
       int pixelIndex = i * 32 +j;
       if ((i % 2 == 0) && (j % 2 != 0)) {
-        pixels.setPixelColor(pixelIndex, pixels.Color(0, 0, 0)); 
+        pixels.setPixelColor(pixelIndex, pixels.Color(0, 0, 0));
       }
       else if ((i % 2 != 0) && (pixelIndex % 2 == 0)) {
-        pixels.setPixelColor(pixelIndex, pixels.Color(80, 0, 0)); 
+        pixels.setPixelColor(pixelIndex, pixels.Color(BRIGHTNESS, 0, 0));
       }
-    
+
   }}
-  pixels.show(); 
+  pixels.show();
 
 }
 
 void transition(int timesRun) {
-    int centerColumn = 15; 
-    int maxWidth = 32; 
+    int centerColumn = 15;
+    int maxWidth = 32;
 
     int maxDistance = max(centerColumn, maxWidth - centerColumn - 1);
 
@@ -357,19 +419,19 @@ void transition(int timesRun) {
             if (leftColumn >= 0) { //light up the left side
                 int pixelIndex = row * maxWidth + leftColumn;
                 if (timesRun % 2 == 0) {
-                  pixels.setPixelColor(pixelIndex, pixels.Color(128, 0, 0)); // can we get a maroon
+                  pixels.setPixelColor(pixelIndex, pixels.Color(BRIGHTNESS, 0, 0)); // can we get a maroon
                 }
                 else  {
-                  pixels.setPixelColor(pixelIndex, pixels.Color(128, 128, 128)); 
+                  pixels.setPixelColor(pixelIndex, pixels.Color(BRIGHTNESS, BRIGHTNESS, BRIGHTNESS));
                 }
             }
             if (rightColumn < maxWidth) { //light up the right side
                 int pixelIndex = row * maxWidth + rightColumn;
                 if (timesRun % 2 == 0) {
-                  pixels.setPixelColor(pixelIndex, pixels.Color(128, 0, 0)); 
+                  pixels.setPixelColor(pixelIndex, pixels.Color(BRIGHTNESS, 0, 0));
                 }
                 else  {
-                  pixels.setPixelColor(pixelIndex, pixels.Color(128, 128, 128)); 
+                  pixels.setPixelColor(pixelIndex, pixels.Color(BRIGHTNESS, BRIGHTNESS, BRIGHTNESS));
                 }
             }
         }
@@ -378,7 +440,7 @@ void transition(int timesRun) {
         pixels.show();
 
 
-        timer(50); 
+        timer(50);
     }
 }
 
@@ -402,14 +464,14 @@ void centerout()  {
         for (int col = 0; col < 32; col++) {
             if (topRow >= 0) { //light up the top side
                 int pixelIndex = topRow * 32 + col;
-                  pixels.setPixelColor(pixelIndex, pixels.Color(9, 9, 9)); 
+                  pixels.setPixelColor(pixelIndex, pixels.Color(BRIGHTNESS, BRIGHTNESS, BRIGHTNESS));
                 }
-            
+
             if (bottomRow < maxHeight) { //light up the bottom side
                 int pixelIndex = bottomRow * 32 + col;
-                  pixels.setPixelColor(pixelIndex, pixels.Color(9, 9, 9)); 
+                  pixels.setPixelColor(pixelIndex, pixels.Color(BRIGHTNESS, BRIGHTNESS, BRIGHTNESS));
             }
-            
+
         }
         pixels.show();
 
@@ -417,17 +479,6 @@ void centerout()  {
         timer(100);
     }
 }
-
-// void drawChar(short num, int digit, unsigned char ir, unsigned char ig, unsigned char ib) {
-//     bool t = (num % 16 > 7) || (num % 16 < -7);
-//     for (unsigned char i=0; i<40; ++i) {
-//       unsigned char j = i;
-//       if ((t && (i % 16 > 7)) || (!t && (i % 16 < 8))) {
-//           j = 7 + (8 * (i / 8)) - i % 8;
-//       }
-//       pixels.setPixelColor(i + num, pixels.Color(ir * numeric[digit][j], ig * numeric[digit][j], ib * numeric[digit][j]));
-//   }
-// }
 
 void scoreboard(int score, int time) {
     // Clear the previous display
@@ -458,7 +509,7 @@ void scoreboard(int score, int time) {
       int timetemp = (time - OnesTime)/10;
       TensTime = timetemp;
     }
-  
+
     // Define the colors for "SCORE" and the number
     unsigned char r = 255, g = 0, b = 0; // Green color
     unsigned char g2 = 255, b2 = 255;
@@ -471,16 +522,16 @@ void scoreboard(int score, int time) {
     drawChar(144, '0' + TensScore, r, g, b);
     //seconds place for score
     drawChar(192, '0' + OnesScore, r, g, b);
-  
 
-  
+
+
     // Update the display to show the new scoreboard
     pixels.show();
 }
 
 
 void level1() {
-  
+
   for (short i = 256; i > -255; i -= 8) {
     pixels.clear();
 
@@ -491,8 +542,8 @@ void level1() {
     drawChar(i + 48 * 4, 'L', 3, 3, 3);
     drawSpace(i + 48 * 5);
     drawChar(i + 48 * 6, '1', 3, 3, 3);
-    
-    
+
+
     pixels.show();
     timer(100);
   }
@@ -500,7 +551,7 @@ void level1() {
 }
 
 void level2() {
-  
+
   for (short i = 256; i > -255; i -= 8) {
     pixels.clear();
 
@@ -512,14 +563,14 @@ void level2() {
     drawSpace(i + 48 * 5);
     drawChar(i + 48 * 6, '2', 3, 3, 3);
 
-    
+
     pixels.show();
     timer(100);
 }}
 
 
 void level3() {
-  
+
   for (short i = 256; i > -255; i -= 8) {
     pixels.clear();
 
@@ -530,8 +581,8 @@ void level3() {
     drawChar(i + 48 * 4, 'L', 3, 3, 3);
     drawSpace(i + 48 * 5);
     drawChar(i + 48 * 6, '3', 3, 3, 3);
-    
-    
+
+
     pixels.show();
     timer(100);
 }}
@@ -580,7 +631,7 @@ void onpress()  {
       i++;
       timer(1000);}
 
-  
+
 }
 
 void countdown()  {
@@ -624,20 +675,20 @@ void loop() {
   // pixels.show();
 
   // countdown();
-  
-  
 
-  //   // if (digitalRead(HOOP_BUTTON) == HIGH) {
-  //   //     for (unsigned char i = 0; i < 255; ++i) {
-  //   //       if (i % 8 == 0) {
-  //   //         pixels.setPixelColor(i, pixels.Color(20, 0, 0));
-  //   //       }
-  //   //     }
-  //   // }
-    
+
+
+//      if (digitalRead(HOOP_BUTTON) == HIGH) {
+//          for (unsigned char i = 0; i < 255; ++i) {
+//            if (i % 8 == 0) {
+//              pixels.setPixelColor(i, pixels.Color(20, 0, 0));
+//            }
+//          }
+//      }
+
   //   pixels.show();
 
-  
+
   // digitalWrite(MOTOR_DIRECTION_IN1, !digitalRead(MOTOR_DIRECTION_IN1));
   // digitalWrite(MOTOR_DIRECTION_IN2, !digitalRead(MOTOR_DIRECTION_IN2));
 
